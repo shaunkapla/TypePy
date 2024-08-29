b8b3848fe40b98cb12a0b83235c4a5a138e09602
Shaun <kaplashaun@gmail.com>
2024-08-29 17:04:03 -0600

    added in decorator function and docstring

--- src/dataclasses/dataclass_decorator.py
+++ src/dataclasses/dataclass_decorator.py
@@ -1,1 +1,23 @@
-# This is a placeholder to track progress
+from dataclasses import fields
+from types import UnionType
+from typing import (
+    get_type_hints,
+    get_args,
+    get_origin
+)
+
+def dataclass_type_check(cls):
+    """
+    This will be the decorator that you will be importing into your module to enforce
+    type checks for python dataclasses
+
+    Example:
+    -------
+    from dataclasses import dataclass
+
+    @dataclass
+    @dataclass_type_check
+    class YourClass:
+        ...
+    """
+    
